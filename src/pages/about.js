07f7656ea031d8462258b186fd8e16be4dcb23b1
Christopher <chris@homecode.co.za>
2020-07-10 12:03:43 +0200

    First commit

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function About() {
+    return (
+        <div style={{color: "teal"}}>
+            <h1>About stuff</h1>
+            <p>Oh cool.</p>
+        </div>
+    )
+}
